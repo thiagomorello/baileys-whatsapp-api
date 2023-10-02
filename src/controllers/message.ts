ebbcd3edc2d0e1d40390684470259ec37406bac7
Thiago Roieski <thiago@thiagomorello.com>
2023-10-02 14:07:52 -0300

    fix

--- src/controllers/message.ts
+++ src/controllers/message.ts
@@ -40,11 +40,8 @@ export const send: RequestHandler = async (req, res) => {
 
     const exists = await jidExists(session, jid, type);
     if (!exists) return res.status(400).json({ error: 'JID does not exists' });
-
-    const newMessage = {
-      text: message,
-    }
-    const result = await session.sendMessage(jid, newMessage, options);
+    
+    const result = await session.sendMessage(jid, message, options);
     res.status(200).json(result);
   } catch (e) {
     const message = 'An error occured during message send';
